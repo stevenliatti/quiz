22bbf33a7deef9535a1ea2f052b05e874e9c70da
raedAbr <abdennadher.raed@gmail.com>
2018-02-21 17:33:17 +0100

    Create android project

---
+++ src/android/YourQuiz/app/src/main/java/hepia/ch/yourquiz/CreateQuizActivity.java
@@ -0,0 +1,13 @@
+package hepia.ch.yourquiz;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CreateQuizActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_quiz);
+    }
+}
