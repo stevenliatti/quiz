00f574d3d0f1f26fa918cf4a3274226946d45b65
Nathanael <nufer.nathanael@gmail.com>
2018-02-25 18:50:45 +0100

    manager questions id fix#1

--- src/client/js/managerFormCreationQuiz.js
+++ src/client/js/managerFormCreationQuiz.js
@@ -1,6 +1,6 @@
-
 var responseId = null;
 var id = null;
+
 /***
  * Constructor of manager of creation form quiz
  * @returns {ManagerFormCreationQuiz}
@@ -14,6 +14,7 @@ function ManagerFormCreationQuiz()
         };
     })();
 
+    this.selectorQuestions = "";
     /***
      * Add a bloc of question in target, and allow to add some responses
      * @param {type} target
@@ -33,33 +34,35 @@ function ManagerFormCreationQuiz()
         })();
 
         if (questionName.val() !== "") {
-            
+
             let questionTarget = "#q" + questionId;
             let responseTarget = "#rq" + questionId;
             let modifyTarget = "#m" + questionId;
 
             /* QUESTION */
+            //
             $(target).append("<div id=\"q" + questionId + "\"></div>");
-            $(questionTarget).append("<label>Question " + questionId + " : <br>" + questionName.val() + "</label><br>");
-            $(questionTarget).append("<input id=\"m" + questionId + "\" type=\"text\">");
+            $(questionTarget).append("<label>Question " + questionId + " :" + "</label><br>");
+            $(questionTarget).append(questionName.val() + "<br>");
 
             if (selectorQuestions !== null) {
                 $(selectorQuestions).append("<option value=\"" + questionName.val() + "\">" + questionName.val() + "</option>");
             }
 
             let rId = responseId();
-            
+
             /* RESPONSES */
+
             $(questionTarget).append("<button type=\"button\"  onclick=\"addResponse('" + target + "','" + responseTarget + "'," + questionId + ")\">Ajouter une réponse</button>");
-            $(questionTarget).append("<button type=\"button\"  onclick=\"deleteQuestion('" + "#q" + questionId + "')\">Supprimer la question</button>");
-            $(questionTarget).append("<button type=\"button\"  onclick=\"deleteResponses('" + "#rq" + questionId + "')\">Supprimer les questions</button>");
+            $(questionTarget).append("<button type=\"button\"  onclick=\"deleteQuestion('" + "#q" + questionId + "','" + target + "')\">Supprimer la question</button>");
+            $(questionTarget).append("<button type=\"button\"  onclick=\"deleteResponses('" + "#rq" + questionId + "')\">Supprimer les réponses</button>");
             $(questionTarget).append('<input id="rq' + questionId + 'Name" type="text">');
             $(questionTarget).append("<div id=\"rq" + questionId + "\"></div>");
 
             /* CLEAR FORM */
             $(questionName).val("");
             $(modifyTarget).hide();
-        }
+    }
     };
 }
 
@@ -68,30 +71,70 @@ function ManagerFormCreationQuiz()
  * @param {type} target
  * @param {type} responseTarget
  * @param {type} questionId
- * @param {type} responseId
  * @returns {undefined}
  */
 function addResponse(target, responseTarget, questionId) {
     let currentId = responseId();
-    let responseName = $("#rq" + questionId + "Name").val();
-    let responseBloc = "#q" + questionId + "_r" + currentId;
-    
+    //let responseName = $("#rq" + questionId + "Name").val();
+    let idBlock = "q" + questionId + "_r" + currentId;
+    let responseBlock = "#" + idBlock;
+
     $(responseTarget).append("<div id=\"q" + questionId + "_r" + currentId + "\"></div>");
-    
-    $(responseBloc).append($("#rq" + questionId + "Name").val());
+    $(responseBlock).append("<button type=\"button\" onclick=\"deleteResponse('" + responseBlock + "','" + responseTarget + "','" + questionId + "')\"> Supprimer </button>");
+    $(responseBlock).append($("#rq" + questionId + "Name").val());
     $("#rq" + questionId + "Name").val("");
-   
 }
 
 /***
  * 
  * @param {type} target
+ * @param {type} questionsTarget
  * @returns {undefined}
  */
-function deleteQuestion(target)
+function deleteQuestion(target, questionsTarget)
 {
-    $(target).empty();
-    id.i--;
+    $(target).remove();
+
+    id = (function () {
+        this.i = 0;
+        return function () {
+            return this.i++;
+        };
+    })();
+
+    $(questionsTarget).children().each(function () {
+        let questionId = id();
+        let newId = "q" + questionId;
+        let newQuestionTarget = "#" + newId;
+        let responseTarget = "#rq" + questionId;
+
+        /* MODIFICATION QUESTION TOOLS*/
+        $(this).attr("id", newId);
+        $(this).children('label').text("Question " + questionId + ":");
+        $(this).find("button:eq(0)").attr("onclick", "addResponse('#" + newId + "','#rq" + questionId + "','" + questionId + "')");
+        $(this).find("button:eq(1)").attr("onclick", "deleteQuestion('" + "#q" + questionId + "','" + "#questions" + "')");
+        $(this).find("button:eq(2)").attr("onclick", "deleteResponses('" + responseTarget + "')");
+        $(this).find("input:eq(0)").attr("id", "rq" + questionId + "Name");
+        $(this).find("div:eq(0)").attr("id", "rq" + questionId);
+
+
+        /* MODIFICATION RESPONSES*/
+        responseId = (function () {
+            let i = 0;
+            return function () {
+                return i++;
+            };
+        })();
+
+        $(this).find("div:eq(0)").children().each(function () {
+            let idResponse = responseId();
+            let newId = "q" + questionId + "_r" + idResponse;
+            $(this).attr("id", newId);
+            $(this).children().each(function () {
+                $(this).attr("onclick", "deleteResponse('#" + newId + "','#rq" + questionId + "','" + questionId + "')");
+            });
+        });
+    });
 }
 
 /***
@@ -102,10 +145,37 @@ function deleteQuestion(target)
 function deleteResponses(target)
 {
     responseId = (function () {
-            let i = 0;
-            return function () {
-                return i++;
-            };
-        })();
-     $(target).empty();
+        let i = 0;
+        return function () {
+            return i++;
+        };
+    })();
+    $(target).empty();
+}
+
+/***
+ * 
+ * @param {type} target
+ * @param {type} responseTarget
+ * @param {type} questionId
+ * @returns {undefined}
+ */
+function deleteResponse(target, responseTarget, questionId)
+{
+    $(target).remove();
+    responseId = (function () {
+        let i = 0;
+        return function () {
+            return i++;
+        };
+    })();
+
+    $(responseTarget).children().each(function () {
+        let idResponse = responseId();
+        let newId = "q" + questionId + "_r" + idResponse;
+        $(this).attr("id", newId);
+        $(this).children().each(function () {
+            $(this).attr("onclick", "deleteResponse('#" + newId + "','#rq" + questionId + "','" + questionId + "')");
+        });
+    });
 }
