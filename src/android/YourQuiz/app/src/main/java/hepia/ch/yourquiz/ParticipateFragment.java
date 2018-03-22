55736947936c3fae6b8f624f26090e7f389b96ac
raedAbr <abdennadher.raed@gmail.com>
2018-03-22 17:37:28 +0100

    Add navigation menu (video tutorial https://www.youtube.com/watch?v=ju837bQOBfg)

---
+++ src/android/YourQuiz/app/src/main/java/hepia/ch/yourquiz/ParticipateFragment.java
@@ -0,0 +1,23 @@
+package hepia.ch.yourquiz;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by raed on 22.03.18.
+ */
+
+public class ParticipateFragment extends Fragment {
+    View view;
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
+        view = inflater.inflate(R.layout.activity_participate_quiz, container, false);
+        return view;
+    }
+}
--- src/android/YourQuiz/app/src/main/java/hepia/ch/yourquiz/ParticipateQuizActivity.java
+++ src/android/YourQuiz/app/src/main/java/hepia/ch/yourquiz/ParticipateQuizActivity.java
@@ -8,23 +8,36 @@ import android.widget.TextView;
 
 public class ParticipateQuizActivity extends AppCompatActivity {
 
-    TextView txtQuestionNumber = (TextView)findViewById(R.id.txtQuestionNumber);
-    TextView txtScore = (TextView)findViewById(R.id.txtScore);
-    TextView txtCoeff = (TextView)findViewById(R.id.txtCoeff);
-    TextView txtQuestion = (TextView)findViewById(R.id.txtQuestion);
-    TextView txtViewTimeLeft = findViewById(R.id.textViewTimeLeft);
+    TextView txtQuestionNumber;
+    TextView txtScore;
+    TextView txtCoeff;
+    TextView txtQuestion;
+    TextView txtViewTimeLeft;
 
-    Button btnAnswerA = findViewById(R.id.buttonAnswerA);
-    Button btnAnswerB = findViewById(R.id.buttonAnswerB);
-    Button btnAnswerC = findViewById(R.id.buttonAnswerC);
-    Button btnAnswerD = findViewById(R.id.buttonAnswerD);
+    Button btnAnswerA;
+    Button btnAnswerB;
+    Button btnAnswerC;
+    Button btnAnswerD;
 
-    ProgressBar progressBarTimeLeft = findViewById(R.id.progressBarTimeLeft);
+    ProgressBar progressBarTimeLeft;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_participate_quiz);
+
+        txtQuestionNumber = findViewById(R.id.txtQuestionNumber);
+        txtScore = findViewById(R.id.txtScore);
+        txtCoeff = findViewById(R.id.txtCoeff);
+        txtQuestion = findViewById(R.id.txtQuestion);
+        txtViewTimeLeft =  findViewById(R.id.textViewTimeLeft);
+
+        btnAnswerA = findViewById(R.id.buttonAnswerA);
+        btnAnswerB = findViewById(R.id.buttonAnswerB);
+        btnAnswerC = findViewById(R.id.buttonAnswerC);
+        btnAnswerD = findViewById(R.id.buttonAnswerD);
+
+        progressBarTimeLeft = findViewById(R.id.progressBarTimeLeft);
     }
 
     private void setQuestionNumber(int current, int total) {
